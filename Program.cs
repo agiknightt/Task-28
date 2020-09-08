8a0f2f96a014c9ba14426719fdb86592547d0742
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-09-08 18:45:19 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(1000, 76, 50);
+
+            player.ShowStats();
+
+            Console.ReadKey();
+        }
+    }
+    class Player
+    {
+        public int Health;
+        public int Damage;
+        public int Armor;
+
+        public Player(int health, int damage, int armor)
+        {
+            Health = health;
+            Damage = damage;
+            Armor = armor;
+        }        
+        public void ShowStats()
+        {
+            Console.WriteLine($"Кол-во жизней - {Health}\nУрон -  {Damage}\nСкорость атаки - {Armor}\n");
+        }
+    }
+}
